7e3bac9348402c08b159665256fce588ae2505ab
Yousef Elserwey <youseef.elserwey@gmail.com>
2025-06-12 16:40:20 +0300

    Migrate from NavigationView to NavigationStack - Replace deprecated NavigationView with modern NavigationStack - Remove platform-specific StackNavigationViewStyle workarounds - Simplifies navigation implementation for iOS 16+ compatibility

--- Sources/Stinsen/NavigationViewCoordinator/NavigationViewCoordinator.swift
+++ Sources/Stinsen/NavigationViewCoordinator/NavigationViewCoordinator.swift
@@ -2,33 +2,22 @@ import Foundation
 import SwiftUI
 
 /// The NavigationViewCoordinator is used to represent a coordinator with a NavigationView
-public class NavigationViewCoordinator<T: Coordinatable>: ViewWrapperCoordinator<T, AnyView> {
+public class NavigationViewCoordinator<T: Coordinatable>: ViewWrapperCoordinator<T, SwiftUI.NavigationStack<NavigationPath, AnyView>> {
     public init(_ childCoordinator: T) {
         super.init(childCoordinator) { view in
-            #if os(macOS)
-            AnyView(
-                NavigationView {
-                    view
-                }
-            )
-            #else
-            AnyView(
-                NavigationView {
-                    view
-                }
-                .navigationViewStyle(StackNavigationViewStyle())
-            )
-            #endif
+            SwiftUI.NavigationStack {
+                view
+            }
         }
     }
     
     @available(*, unavailable)
-    public override init(_ childCoordinator: T, _ view: @escaping (AnyView) -> AnyView) {
+    public override init(_ childCoordinator: T, _ view: @escaping (AnyView) -> SwiftUI.NavigationStack<NavigationPath, AnyView>) {
         fatalError("view cannot be customized")
     }
     
     @available(*, unavailable)
-    public override init(_ childCoordinator: T, _ view: @escaping (any Coordinatable) -> (AnyView) -> AnyView) {
+    public override init(_ childCoordinator: T, _ view: @escaping (any Coordinatable) -> (AnyView) -> SwiftUI.NavigationStack<NavigationPath, AnyView>) {
         fatalError("view cannot be customized")
     }
 }
